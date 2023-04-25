b33ea4d81851ad3d72fec527f407654b8939471f
Kaiwarre <ainaw4265@gmail.com>
2023-04-25 20:40:10 +0600

    add pdf

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,13 @@ class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = ('title', 'pdf_file')
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'style': 'background-color: white',
+                'class': 'form-control',
+                'placeholder': 'Название файла'}),
+            'pdf_file': forms.FileInput(attrs={
+                'style': 'background-color: white',
+                'class': 'form-control',
+                'placeholder': 'Название файла'}),
+        }
